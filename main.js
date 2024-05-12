70f778aac1153faa2329d09207d229c971a46825
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2024-05-12 12:04:31 +1000

    Added system tray support

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const { app, BrowserWindow, session } = require('electron');
+const { app, BrowserWindow, session, Tray, nativeImage } = require('electron');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config(); // Load environment variables from .env file
 
 let mainWindow;
-
-let appUrl = 'https://chatgpt.com/';
-let rootAppUrl = '.chatgpt.com';
+let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
@@ -23,12 +24,12 @@ app.on('ready', () => {
         if (url !== "about:blank#blocked") electron.shell.openExternal(url);
     });
 
-    mainWindow.loadURL(appUrl, {userAgent: 'Chrome'});
+    mainWindow.loadURL(process.env.APP_URL, {userAgent: 'Chrome'}); // Use environment variable
 
     // Listen for authentication-related cookies
     session.defaultSession.cookies.on('changed', (event, cookie, cause, removed) => {
-        if (!removed && cookie.domain.includes(rootAppUrl)) {
-            // Store the authentication-related cookie
+        if (!removed) { // Use environment variable
+            // Store the authentication
             app.setLoginItemSettings({
                 openAtLogin: true,
                 path: app.getPath('userData'),
@@ -41,5 +42,31 @@ app.on('ready', () => {
 
     mainWindow.on('closed', () => {
         mainWindow = null;
+    });
+
+    // Create a tray icon based on the current platform
+    if (process.platform === 'darwin') {
+        tray = new Tray(nativeImage.createFromPath(path.join(__dirname, 'icon@2x.png')));
+    } else if (process.platform === 'win32') {
+        // https://www.xiconeditor.com/
+        tray = new Tray(nativeImage.createFromPath(path.join(__dirname, 'favicon.ico')));
+    }
+
+    tray.setToolTip(process.env.APP_NAME); // Set tooltip for the tray icon
+
+    // Minimize to tray when window is minimized
+    mainWindow.on('minimize', function (event) {
+        event.preventDefault();
+        mainWindow.hide();
+    });
+
+    // Restore window when tray icon is clicked
+    tray.on('click', function () {
+        mainWindow.show();
+    });
+
+    // Quit application when tray icon is right-clicked
+    tray.on('right-click', () => {
+        app.quit();
     });
 });
